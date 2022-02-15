16ac81ed9325c9aab5615c80d2557bc0816ced9e
sergey <petrelevich@yandex.ru>
2022-02-15 10:49:02 +0300

    L22 update weakHashMap example

--- L22-cache/src/main/java/ru/otus/references/WeakMapDemo.java
+++ L22-cache/src/main/java/ru/otus/references/WeakMapDemo.java
@@ -11,14 +11,19 @@ public class WeakMapDemo {
 
     public static void main(String[] args) throws InterruptedException {
         new WeakMapDemo().start();
-       // new WeakMapDemo().startStrange();
+        // new WeakMapDemo().startStrange();
     }
 
     private void start() throws InterruptedException {
         Map<String, Integer> cache = new WeakHashMap<>();
         var limit = 100;
+
+        var d = "key:21";  // для для демонстрации intern
+        var d2 = "key:22"; // для для демонстрации intern
         for (var idx = 0; idx < limit; idx++) {
-            cache.put("key:" + idx, idx);
+            var key = ("key:" + idx).intern(); // останутся значения для "key:21" и "key:22"
+            // var key = "key:" + idx; // при таком подходе значение удалится.
+            cache.put(key, idx);
         }
 
         logger.info("before gc: {}", cache.size());
